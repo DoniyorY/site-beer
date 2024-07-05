e8be29d7b1d44fc02e1d0d7d12aa456522bea610
V1CTUS <yusupovdoniyor54@gmail.com>
2024-07-05 15:39:33 +0500

    styles.css

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -6,77 +6,77 @@ return [
     'Our Menu' => [
         'ru' => 'НАШЕ МЕНЮ',
         'en' => 'Our Menu',
-        'uz' => '',
+        'uz' => 'BIZNING MENU',
     ],
     'Events' => [
         'ru' => 'МЕРОПРИЯТИЯ',
         'en' => 'Events',
-        'uz' => '',
+        'uz' => 'TADBIRLAR',
     ],
     'Beers' => [
         'ru' => 'ПИВО',
         'en' => 'Beers',
-        'uz' => '',
+        'uz' => 'PIVO',
     ],
     'Contact' => [
         'ru' => 'КОНТАКТЫ',
         'en' => 'Contact',
-        'uz' => '',
+        'uz' => 'ALOQA UCHUN',
     ],
     'Home' => [
         'ru' => 'Главная',
         'en' => 'Home',
-        'uz' => '',
+        'uz' => 'BOSH SAHIFA',
     ],
     'VOTE FOR US' => [
         'ru' => 'ГОЛОСУЙТЕ ЗА НАС',
         'en' => 'VOTE FOR US',
-        'uz' => '',
+        'uz' => 'BIZGA OVOZ BERING',
     ],
     'Information' => [
         'ru' => 'ИНФОРМАЦИЯ',
         'en' => 'Information',
-        'uz' => '',
+        'uz' => 'MA `LUMOT',
     ],
     'Contact &amp; visit' => [
         'ru' => 'Связаться и посетить',
         'en' => 'Contact &amp; visit',
-        'uz' => '',
+        'uz' => 'Aloqa qiling va tashrif buyuring',
     ],
     'Privacy' => [
         'ru' => 'Конфиденциальность',
         'en' => 'Privacy',
-        'uz' => '',
+        'uz' => 'Maxfiylik',
     ],
     'Address' => [
         'ru' => 'АДРЕС',
         'en' => 'Address',
-        'uz' => '',
+        'uz' => 'MANZIL',
     ],
     'Rowing canal' => [
         'ru' => 'ГРЕБНОЙ КАНАЛ',
         'en' => 'Rowing canal',
-        'uz' => '',
+        'uz' => 'ESHKAK ESHISH KANALI',
     ],
     'Samarkand' => [
         'ru' => 'САМАРКАНД',
         'en' => 'Samarkand',
-        'uz' => '',
+        'uz' => 'SAMARQAND',
     ],
     'Uzbekistan' => [
         'ru' => 'УЗБЕКИСТАН',
         'en' => 'Uzbekistan',
-        'uz' => '',
+        'uz' => "O'ZBEKISTON",
     ],
     'tel:' => [
         'ru' => 'тел:',
         'en' => 'tel:',
-        'uz' => '',
+        'uz' => 'tel:',
     ],
     'Opening hours' => [
         'ru' => 'ВРЕМЯ РАБОТЫ',
         'en' => 'Opening hours',
-        'uz' => '',
+        'uz' => 'ISH VAQTI',
     ],
     'footer_schedule' => [
         'ru' => '<p class="mt-3">
@@ -99,7 +99,18 @@ return [
                         Saturday &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;11:00 – 01:00<br>
                         Sunday &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;11:00 – 01:00
                     </p>',
-        'uz' => '',
+        'uz' => '
+        <p class="mt-3">
+                        Dushanba &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 11:00 – 01:00<br>
+                        Seshanba &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; 11:00 – 01:00<br>
+                        Chorshanba &nbsp; &nbsp;&nbsp; &nbsp;11:00 – 01:00<br>
+                        Payshanba &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;11:00 – 01:00<br>
+                        Juma &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;11:00 –
+                        01:00<br>
+                        Shanba &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;11:00 – 01:00<br>
+                        Yakshanba &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;11:00 – 01:00
+                    </p>
+        ',
     ],
     'banner_title' => [
         'ru' => '<h1 style="text-align: center"><span class="white">История и искусство пивоварения:</span>
@@ -107,7 +118,11 @@ return [
             <span class="yellow" style="color: #FFA025">От традиций до современных тенденций</span>
         </h1>',
         'en' => '',
-        'uz' => '',
+        'uz' => '
+        <h1 style="text-align: center"><span class="white">Pivo tayyorlash tarixi va san\'ati:</span>
+            <br>
+            <span class="yellow" style="color: #FFA025">an\'analardan zamonaviy tendentsiyalargacha</span>
+        </h1>',
     ],
     'about_title' => [
         'ru' => '
@@ -117,37 +132,40 @@ return [
         'en' => '<h2 class="text-center ">
                     <span> The Brewery\'s History: </span> <span class="yellow"> From Origins to the Present Day </span>
                 </h2>',
-        'uz' => '',
+        'uz' => '
+        <h2 class="text-center ">
+                    <span> Pivo zavodining tarixi: </span> <span class="yellow"> kelib chiqishidan hozirgi kungacha </span>
+                </h2>',
     ],
     'about_short' => [
         'ru' => 'Пивоварня "Хмельнов" начала свою работу в 2021 году. С тех пор мы неустанно совершенствуем наши рецепты и технологии, чтобы радовать наших покупателей отличным пивом.',
         'en' => 'Khmelnov Brewery began its operations in 2021. Since then, we have been tirelessly improving our recipes and technologies to delight our customers with excellent beer.',
-        'uz' => '',
+        'uz' => "Xmelnov pivo fabrikasi o'z ishini 2021-yilda boshlagan. O'shandan beri biz mijozlarimizni ajoyib pivo bilan xursand qilish uchun retseptlarimiz va texnologiyalarimizni tinimsiz takomillashtirmoqdamiz.",
     ],
     'maps_title' => [
         'ru' => 'Оборудование компании "Чешский пивовар"',
         'en' => 'The equipment of the company "Czech Brewmaster"',
-        'uz' => '',
+        'uz' => '"Czech Brewermaster" kompaniyasining uskunalari',
     ],
     'left_map_title' => [
         'ru' => 'География сырьевых ресурсов',
         'en' => 'GEOGRAPHY OF RAW MATERIALS',
-        'uz' => '',
+        'uz' => 'Xom ashyo geografiyasi',
     ],
     'left_map_content' => [
         'ru' => 'Для приготовления пива мы используем высококачественные ингредиенты из разных регионов мира. Солод и хмель поставляются из лучших пивоваренных регионов Новой Зеландии, Бельгии, Франции, Дании, Германии, Австрии, США и России, что позволяет нам добиваться уникальных вкусовых характеристик.',
         'en' => 'For brewing our beer, we use high-quality ingredients from various regions around the world. Our malt and hops are sourced from the best brewing regions in New Zealand, Denmark, Germany, Austria, USA, and Russia, allowing us to achieve unique flavor characteristics.',
-        'uz' => '',
+        'uz' => 'Pivoni tayyorlash uchun biz dunyoning turli mintaqalaridan yuqori sifatli ingredientlardan foydalanamiz. Malt va hoplar Yangi Zelandiya, Belgiya, Fransiya, Daniya, Germaniya, Avstriya, AQSH va Rossiyaning eng yaxshi pivo ishlab chiqaruvchi mintaqalaridan olinadi, bu bizga noyob lazzat profillariga erishish imkonini beradi.',
     ],
     'Current Events' => [
         'ru' => 'Актуальные мероприятия',
         'en' => 'Current Events',
-        'uz' => '',
+        'uz' => 'JORIY TADBIRLAR',
     ],
     'Jazz evening' => [
         'ru' => 'Вечер джаза',
         'en' => 'Jazz evening',
-        'uz' => '',
+        'uz' => 'JAZZ OQSHOMI',
     ],
     'jazz_content' => [
         'ru' => 'Присоединяйтесь к нам, чтобы провести незабываемый вечер джаза в нашем пивном ресторане! Погрузитесь в вечер плавных мелодий, вкусного крафтового пива и теплой, гостеприимной атмосферы. В [дата] мы с радостью примем у себя исключительное живое джазовое выступление, которое обещает очаровать и развлечь.',
@@ -155,17 +173,17 @@ return [
                                 evening of smooth melodies, delicious craft beers, and a warm, inviting atmosphere. On
                                 [date], we are excited to host an exceptional live jazz performance that promises to
                                 captivate and entertain.',
-        'uz' => '',
+        'uz' => "Restobarimizda unutilmas jazz oqshomiga qo'shiling! Yoqimli oqshom ohanglari, mazali pivo va iliq, mehmondo'st muhitga sho'ng'ing. [Sana] kuni biz jozibador va ko'ngilochar ajoyib jonli jazz ijrosini o'tkazishdan mamnunmiz.",
     ],
     'jazz_schedule' => [
         'ru' => ' <p> Каждую субботу <br> 20:00 </p>',
         'en' => ' <p> Every Saturday <br> 20:00 </p>',
-        'uz' => '',
+        'uz' => ' <p> HAR SHANBA <br> 20:00 </p>',
     ],
     'Lamb Shank in Cream Sauce' => [
         'ru' => 'Голень ягненка в сливочном соусе',
         'en' => 'Lamb Shank in Cream Sauce',
-        'uz' => '',
+        'uz' => "QAYMOQLI sousdagi qo'zicjoq go'shti",
     ],
     'restaurant_content' => [
         'ru' => 'Наша фирменная голень ягненка в сливочном соусе - это кулинарный шедевр, созданный для истинных гурманов. Нежность ягненка в сочетании с богатством сливочного соуса порадует даже самые взыскательные вкусы. Медленно тушеная до готовности, голень ягненка щедро поливается ароматным сливочным соусом, обещая взрыв вкусов и незабываемые гастрономические впечатления.',
@@ -173,22 +191,22 @@ return [
                         tenderness of the lamb combined with the richness of the cream sauce will delight even the most
                         discerning palates. Slowly braised until tender, the lamb shank is generously drenched in a
                         fragrant cream sauce, promising a burst of flavors and unforgettable gastronomic experiences.',
-        'uz' => '',
+        'uz' => "Bizning qaymoqli sousdagi qo'zichoq go'shti haqiqiy gurmelar uchun yaratilgan oshpazlik asaridir. Qo'zichoq go'shtining nozikligi qaymoqli sousning boyligi bilan birlashtirilib, hatto eng talabchan tanglayni ham xursand qiladi. Sekin-asta qaynatilguncha qaynatilgan qo'zichoq go'shti boy, qaymoqli sousga qo'shib, ajoyib ta'm va unutilmas ovqatlanish tajribasini va'da qiladi.",
     ],
     'Find Out more about our menu' => [
         'ru' => 'узнайте больше о нашем меню',
         'en' => 'Find Out more about our menu',
-        'uz' => '',
+        'uz' => "bizning menyu haqida ko'proq bilib oling",
     ],
     'Hold an event with us' => [
         'ru' => 'проведите мероприятие вместе с нами',
         'en' => 'Hold an event with us',
-        'uz' => '',
+        'uz' => "biz bilan tadbir o'tkazing",
     ],
     'Brewery' => [
         'ru' => 'наши сорта пива',
         'en' => 'Brewery',
-        'uz' => '',
+        'uz' => "bizning pivo navlarimiz",
     ],
     '' => [
         'ru' => '',
